d4e00649f52b3467c930386dea452dc7cba79130
Freezingmoon98 <anna-lena.vogelgesang@hydac.com>
2023-10-24 08:10:19 +0000

    Added Logout

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.decorators import login_required
 from item.models import Category,Item
 from .forms import SignupForm
-# Create your views here.
+from django.contrib.auth import logout
+
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -12,10 +14,12 @@ def index(request):
         'items': items,
     })
 
+#for contact page
 def contact(request):
     return render(request, 'core/contact.html')
 
-def signup(request): #create user, if user=valid => saved in db
+ #create user, if user=valid => saved in db
+def signup(request):
     if request.method == 'POST':
        form = SignupForm(request.POST)
 
@@ -29,3 +33,10 @@ def signup(request): #create user, if user=valid => saved in db
     return render(request, 'core/signup.html', {
         'form': form
     })
+
+#logout user, if logout button is pressed 
+@login_required
+def lgout(request):
+
+    logout(request)
+    return redirect('/')
